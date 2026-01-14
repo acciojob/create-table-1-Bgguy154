aff4ca94ae3e45db4d038e6111aa0605f035ec0f
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2026-01-14 13:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+    const table=document.querySelector("#sampleTable");
+	const tr=document.createElement("tr");
+	const td1=document.createElement("td");
+	const td2=document.createElement("td");
+	td1.textContent="New Cell1";
+	td2.textContent="New Cell2";
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	table.insertBefore(tr,table.firstElementChild);
   
 }
