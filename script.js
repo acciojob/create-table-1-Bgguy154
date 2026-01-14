21b8bae1997d7d2c7e213bb4affd1b75e994c661
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2026-01-14 14:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 function insert_Row() {
     //Write your code here
     const table=document.querySelector("#sampleTable");
-	const tr=document.createElement("tr");
-	const td1=document.createElement("td");
-	const td2=document.createElement("td");
+	const tr=table.insertRow(0);
+	const td1=tr.insertCell(0);
+	const td2=tr.insertCell(1);
 	td1.textContent="New Cell1";
 	td2.textContent="New Cell2";
-	tr.appendChild(td1);
-	tr.appendChild(td2);
-    if (table.firstElementChild) {
-    table.insertBefore(tr, table.firstElementChild);
-} else {
-    table.appendChild(tr);
-}
 }
